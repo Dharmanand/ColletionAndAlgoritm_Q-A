dba6e50a2bb30d84929e421799ecb12de61fc530
Dharmanand <kumar.kd20@gmail.com>
2018-12-08 17:12:30 +0530

    Added method for getSortedMapByKey(Map<Integer, Integer> map)

--- src/com/kd/qa/map/SortHashMapByKeyAndValue.java
+++ src/com/kd/qa/map/SortHashMapByKeyAndValue.java
@@ -1,10 +1,37 @@
 package com.kd.qa.map;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+// How to sort HashMap by key and value in Java - Hashtable, Map Example Tutorial
+
 public class SortHashMapByKeyAndValue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(101, 1001);
+		map.put(108, 1008);
+		map.put(102, 1002);
+		map.put(109, 1009);
+		map.put(105, 1005);
+		map.put(104, 1004);
+		System.out.println(getSortedMapByKey(map));
 	}
 
+	public static Map<Integer, Integer> getSortedMapByKey(Map<Integer, Integer> map) {
+
+		LinkedHashMap<Integer, Integer> temp = new LinkedHashMap<>();
+		
+		List<Integer> list = new ArrayList<>(map.keySet());
+		Collections.sort(new ArrayList<>(map.keySet()));
+
+		for (Integer i : list)
+			temp.put(i, map.get(i));
+		return temp;
+	}
 }
